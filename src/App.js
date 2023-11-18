1de575f76242f5de96c8536acb28bddcd95174ac
Yash <kudveyash@gmail.com>
2023-11-18 17:07:45 +0530

    Minor changes and state management

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import Search from './components/Search/Search';
 
 function App() {
+  const handleOnSearchChange = (searchdata) => {
+    console.log(searchdata)
+  }
   return (
     <div className="container">
-      <Search />
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   );
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AsyncPaginate } from "react-select-async-paginate";
 
-const Search = () => {
+const Search = (onSearchChange) => {
+
+    const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValue) => {
+
+    }
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
     return (
-        <div>
-            hello
-        </div>
+        <AsyncPaginate placeholder='Search for city' debounceTimeout={600} value={search} onChange={handleOnChange} loadOptions={loadOptions} />
     )
 }
 
